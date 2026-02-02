6628b65108647b3eb0c48699c5f6af068beadab3
NoSa1e <cjsp1217@gmail.com>
2026-02-02 15:46:54 +0900

    [chore] #0 project

---
+++ freebridge/src/test/java/com/fallguys/freebridge/FreebridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fallguys.freebridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreebridgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
