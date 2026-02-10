1b598c8e4fb8abc896c49296d2901e224677f419
NoSa1e <cjsp1217@gmail.com>
2026-02-10 13:32:20 +0900

    HFP-40-feat-module create

--- freebridge/recruitment/src/main/java/com/ohgiraffers/recruitment/domain/enums/JobPostingStatus.java
+++
@@ -1,6 +0,0 @@
-package com.ohgiraffers.recruitment.domain.enums;
-
-public enum JobPostingStatus {
-    OPEN,
-    CLOSED
-}
--- freebridge/recruitment/src/main/java/com/ohgiraffers/recruitment/domain/enums/ProjectStatus.java
+++
@@ -1,7 +0,0 @@
-package com.ohgiraffers.recruitment.domain.enums;
-
-public enum ProjectStatus {
-    IN_PROGRESS,
-    COMPLETED,
-    CANCELLED
-}
