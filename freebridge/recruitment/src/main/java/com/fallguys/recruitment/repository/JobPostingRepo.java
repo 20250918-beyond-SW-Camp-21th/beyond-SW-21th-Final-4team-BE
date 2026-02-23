972420fc8de93e57a88f516695731ac532a6ba3a
이용우 <qhtm008@naver.com>
2026-02-23 19:47:08 +0900

    [feat] @Repository

--- freebridge/recruitment/src/main/java/com/fallguys/recruitment/repository/JobPostingRepo.java
+++ freebridge/recruitment/src/main/java/com/fallguys/recruitment/repository/JobPostingRepo.java
@@ -2,7 +2,9 @@ package com.fallguys.recruitment.repository;
 
 import com.fallguys.recruitment.entity.JobPosting;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface JobPostingRepo extends JpaRepository<JobPosting, Long> {
 
 }
