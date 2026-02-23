3e5cda8091bef88f9e45cf1e98e97255ee269628
이용우 <qhtm008@naver.com>
2026-02-23 12:44:15 +0900

    [feat] CreateJobPosting Service

--- freebridge/recruitment/src/main/java/com/fallguys/recruitment/api/dto/request/JobPostingCreateDTO.java
+++ freebridge/recruitment/src/main/java/com/fallguys/recruitment/api/dto/request/JobPostingCreateDTO.java
@@ -1,8 +1,11 @@
 package com.fallguys.recruitment.api.dto.request;
 
+import lombok.Getter;
+
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Getter
 public class JobPostingCreateDTO {
     Long employerId;
     String employerName;
--- freebridge/recruitment/src/main/java/com/fallguys/recruitment/service/JobPostingServiceImpl.java
+++ freebridge/recruitment/src/main/java/com/fallguys/recruitment/service/JobPostingServiceImpl.java
@@ -1,6 +1,7 @@
 package com.fallguys.recruitment.service;
 
 import com.fallguys.recruitment.api.dto.request.JobPostingCreateDTO;
+import com.fallguys.recruitment.entity.JobPosting;
 import com.fallguys.recruitment.repository.JobPostingRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -14,6 +15,15 @@ public class JobPostingServiceImpl implements JobPostingService {
 
     @Override
     public void createJobPosting(JobPostingCreateDTO jobPostingCreateDTO){
+        JobPosting jobPosting = JobPosting.builder()
+                .employerId(jobPostingCreateDTO.getEmployerId())
+                .employerName(jobPostingCreateDTO.getEmployerName())
+                .title(jobPostingCreateDTO.getTitle())
+                .description(jobPostingCreateDTO.getDescription())
+                .techStack(jobPostingCreateDTO.getTechStack())
+                .duration(jobPostingCreateDTO.getDuration())
+                .createdAt(jobPostingCreateDTO.getCreatedAt()).build();
 
+        jobPostingRepo.save(jobPosting);
     }
 }
