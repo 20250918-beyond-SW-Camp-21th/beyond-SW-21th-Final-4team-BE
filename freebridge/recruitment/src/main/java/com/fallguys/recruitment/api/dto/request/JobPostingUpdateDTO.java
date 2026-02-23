162d7648b3246a7c0a1dfb3076cff2539192d6ab
이용우 <qhtm008@naver.com>
2026-02-23 14:31:01 +0900

    [feat] JobPostingUpdateDTO

--- freebridge/recruitment/src/main/java/com/fallguys/recruitment/api/dto/request/JobPostingUpdateDTO.java
+++ freebridge/recruitment/src/main/java/com/fallguys/recruitment/api/dto/request/JobPostingUpdateDTO.java
@@ -2,8 +2,15 @@ package com.fallguys.recruitment.api.dto.request;
 
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Getter
 public class JobPostingUpdateDTO {
-    JobPostingCreateDTO jobPostingCreateDTO;
-
+    String title;
+    String description;
+    List<String> techStack;
+    Long budget;
+    Integer duration;
+    LocalDateTime updatedAt=LocalDateTime.now();
 }
