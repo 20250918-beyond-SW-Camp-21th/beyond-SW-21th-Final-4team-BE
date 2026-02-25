1ccc05ddc00f669535c3860239387024f0e4d725
danielhw99 <danielhw99@gmail.com>
2026-02-25 10:03:57 +0900

    HFP-24 [feat] 마이페이지 PortfolioInfo.java 자료형변경

--- freebridge/user/src/main/java/com/fallguys/mypage/entity/freelancer/PortfolioInfo.java
+++ freebridge/user/src/main/java/com/fallguys/mypage/entity/freelancer/PortfolioInfo.java
@@ -6,13 +6,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Embeddable
 @Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class PortfolioInfo {
     private String portfolioFileUrl;
     private String portfolioFileName;
-    private String portfolioLastUpdated;
+    private LocalDateTime portfolioLastUpdated;
 }
