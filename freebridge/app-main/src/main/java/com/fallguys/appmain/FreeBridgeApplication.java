58f5df135fbd4c17bd73f820954dcde3f382180f
danielhw99 <danielhw99@gmail.com>
2026-02-25 15:50:09 +0900

    HFP-72 [chore] Swagger Configuration
    - Swagger 의존성 추가 및 기본 설정

--- freebridge/app-main/src/main/java/com/fallguys/appmain/FreeBridgeApplication.java
+++ freebridge/app-main/src/main/java/com/fallguys/appmain/FreeBridgeApplication.java
@@ -2,10 +2,12 @@ package com.fallguys.appmain;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
-@ComponentScan(basePackages = {"com.fallguys.common.config, com.fallguys.config"})
+@SpringBootApplication(scanBasePackages = "com.fallguys")
+@EntityScan(basePackages = "com.fallguys")
+@EnableJpaRepositories(basePackages = "com.fallguys")
 public class FreeBridgeApplication {
     public static void main(String[] args) {
         SpringApplication.run(FreeBridgeApplication.class, args);
--- freebridge/user/src/main/java/com/fallguys/config/SwaggerConfig.java
+++ freebridge/user/src/main/java/com/fallguys/config/SwaggerConfig.java
@@ -10,11 +10,24 @@ public class SwaggerConfig implements SwaggerConfigInterface {
 
     @Bean
     public GroupedOpenApi userGroupedOpenApi() {
-        return createGroupedOpenApi("user", "/user/**", "USER API", "USER Domain API");
+        return createGroupedOpenApi("user", "/api/users/**", "USER API", "USER Domain API");
     }
 
     @Bean
     public GroupedOpenApi mypageGroupedOpenApi() {
-        return createGroupedOpenApi("mypage", "/api/mypage/**", "MYPAGE API", "MYPAGE Domain API");
+        return GroupedOpenApi.builder()
+                .group("mypage")
+                .pathsToMatch(
+                        "/api/employer/mypage/**",
+                        "/api/freelancer/mypage/**"
+                )
+                .addOpenApiCustomizer(openApi ->
+                        openApi.setInfo(new io.swagger.v3.oas.models.info.Info()
+                                .title("MYPAGE API")
+                                .description("MYPAGE Domain API")
+                                .version("1.0.0")
+                        )
+                )
+                .build();
     }
 }
