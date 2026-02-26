ef8025a26d2a4c5695374c5445461ab9a5a684a5
이용우 <qhtm008@naver.com>
2026-02-26 12:09:09 +0900

    HFP-73 [fix] 지원 생성 요청 DTO 유효성

--- freebridge/matchs/src/main/java/com/fallguys/matchs/api/web/MatchsController.java
+++ freebridge/matchs/src/main/java/com/fallguys/matchs/api/web/MatchsController.java
@@ -9,6 +9,7 @@ import com.fallguys.matchs.api.dto.response.ProposalResponseDTO;
 import com.fallguys.matchs.service.MatchsService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -33,7 +34,7 @@ public class MatchsController {
     @PostMapping("/api/v1/freelancer/application")
     public ResponseEntity<ApiResponse<Map<String, Long>>> createApplication(
             @RequestParam Long freelancerId,
-            @RequestBody ApplicationCreateRequest request
+            @Valid @RequestBody ApplicationCreateRequest request
     ) {
         Long applicationId = matchsService.createApplication(freelancerId, request);
         return ResponseEntity.ok(ApiResponse.ok(Map.of("applicationId", applicationId)));
