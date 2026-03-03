ecfd10c7adb0ce50010df87ed069f690350b444b
danielhw99 <danielhw99@gmail.com>
2026-03-03 16:36:16 +0900

    HFP-92 [fix] CodeRabbit 피드백 반영: Resume 엔티티 freelancer_id에 대한 명시적 DB-level Unique 제약(uq_resume_freelancer_id) 추가

--- freebridge/user/src/main/java/com/fallguys/mypage/entity/resume/Resume.java
+++ freebridge/user/src/main/java/com/fallguys/mypage/entity/resume/Resume.java
@@ -14,7 +14,9 @@ import java.util.List;
 
 @Entity
 @Getter
-@Table(name = "resume")
+@Table(name = "resume", uniqueConstraints = {
+        @UniqueConstraint(name = "uq_resume_freelancer_id", columnNames = {"freelancer_id"})
+})
 @EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor
 public class Resume {
