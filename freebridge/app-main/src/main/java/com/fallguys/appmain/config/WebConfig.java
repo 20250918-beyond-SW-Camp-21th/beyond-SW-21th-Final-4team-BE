171f97f79ccfe9863bc9e8adcc413cb1bddfce50
Eric Y <eyoun17@gmail.com>
2026-03-05 19:49:45 +0900

    HFP-103 [fix] 포트번호 수정 #127

--- freebridge/app-main/src/main/java/com/fallguys/appmain/config/WebConfig.java
+++ freebridge/app-main/src/main/java/com/fallguys/appmain/config/WebConfig.java
@@ -9,8 +9,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // 모든 경로에 대해
-                .allowedOrigins("http://localhost:3000") // 프론트엔드 주소 (개발 시 보통 3000)
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
