804c5fdd9f372a82e7e6732b36d40eecb32d83ed
danielhw99 <danielhw99@gmail.com>
2026-03-05 10:29:30 +0900

    HFP-88 [feat] 구독 - 고용주 구독 정보 업데이트 예약 확인 스케줄러
    - 구독 Downgrade의 경우 다음 결제일 전 날까지 구독이 유지되어, schedular를 통해 구독 정보 확인을 하여 업데이트 해준다.

--- freebridge/user/src/main/java/com/fallguys/mypage/repository/employer/EmployerRepository.java
+++ freebridge/user/src/main/java/com/fallguys/mypage/repository/employer/EmployerRepository.java
@@ -4,13 +4,15 @@ import com.fallguys.mypage.entity.employer.Employer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface EmployerRepository extends JpaRepository<Employer, Long> {
 
-    Optional<Employer> findByEmployerId(Long employerId);
-
     Optional<Employer> findByUserId(Long userId);
-    
+
+    // 예약된 구독 변경건(다운그레이드) 중, 적용일이 지났거나 도래한 목록 조회
+    List<Employer> findByPendingSubscriptionIsNotNullAndPlanChangeEffectiveDateLessThanEqual(LocalDateTime effectiveDate);
 }
