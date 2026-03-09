db36a4af12799d6fb7b06a99c1ddecf6a0d60b2f
JAEWOO JUNG <165545303+NoSa1e@users.noreply.github.com>
2026-03-09 11:48:24 +0900

    Revert "HFP-102 [fix] "비밀번호 변경로직 수정"" (#138)

--- freebridge/user/src/main/java/com/fallguys/user/api/web/dto/response/UserResponseDto.java
+++ freebridge/user/src/main/java/com/fallguys/user/api/web/dto/response/UserResponseDto.java
@@ -14,7 +14,6 @@ public class UserResponseDto {
     private Long id;
     private String email;
     private String name;
-    private String phone;
     private Role role;
     private Boolean termsAgreed;
     private Boolean privacyAgreed;
@@ -29,7 +28,6 @@ public class UserResponseDto {
                 .id(user.getId())
                 .email(user.getEmail())
                 .name(user.getName())
-                .phone(user.getPhone())
                 .role(user.getRole())
                 .termsAgreed(user.getTermsAgreed())
                 .privacyAgreed(user.getPrivacyAgreed())
