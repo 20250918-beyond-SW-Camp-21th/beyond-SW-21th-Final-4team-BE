47317c7f87d79c9b9441c3099009e681cd9fcd75
JAEWOO JUNG <165545303+NoSa1e@users.noreply.github.com>
2026-03-11 18:33:29 +0900

    Revert "HFP-99[fix] 비동기처리 Redis 추가"

--- freebridge/recruitment/src/main/java/com/fallguys/recruitment/service/JobPostingService.java
+++ freebridge/recruitment/src/main/java/com/fallguys/recruitment/service/JobPostingService.java
@@ -44,8 +44,4 @@ public interface JobPostingService {
     void completeProject(Long projectId, Long userId);
 
     void refreshEmployerProjectStatsCache(Long employerId);
-
-    void triggerFreelancerRecommendation(Long jobPostingId, Long userId);
-
-    void triggerJobRecommendation(Long userId);
 }
