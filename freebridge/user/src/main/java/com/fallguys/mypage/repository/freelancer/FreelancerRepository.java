b4ddd006ba984eff2956a1fbc604403f9c0195df
nosale <aoanf12@gmail.com>
2026-03-19 00:14:56 +0900

    HFP-121 [ref] 백필 완료 후 로직 제거

--- freebridge/user/src/main/java/com/fallguys/mypage/repository/freelancer/FreelancerRepository.java
+++ freebridge/user/src/main/java/com/fallguys/mypage/repository/freelancer/FreelancerRepository.java
@@ -4,7 +4,6 @@ import com.fallguys.mypage.entity.freelancer.Freelancer;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -20,9 +19,6 @@ public interface FreelancerRepository extends JpaRepository<Freelancer, Long> {
     Optional<Freelancer> findByUserId(Long userId);
 
     List<Freelancer> findAllByUserIdIn(Collection<Long> userIds);
-
-    @EntityGraph(attributePaths = "skills")
-    List<Freelancer> findByFreelancerIdGreaterThanOrderByFreelancerIdAsc(Long freelancerId, Pageable pageable);
 
     @Query(
             value = """
